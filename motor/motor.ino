cb4f070b7d8e50b205c9ed72045c30616297c7e7
cleuton.sampaio@gmail.com <cleuton@MBP-de-CLEUTON.home>
2018-02-25 07:54:41 -0300

    Teste motor

---
+++ motor/motor.ino
@@ -0,0 +1,52 @@
+void setup() {
+  // put your setup code here, to run once:
+//Setup Channel A
+  pinMode(12, OUTPUT); //Initiates Motor Channel A pin
+  pinMode(9, OUTPUT); //Initiates Brake Channel A pin
+  //Setup Channel B
+  pinMode(13, OUTPUT); //Initiates Motor Channel A pin
+  pinMode(8, OUTPUT);  //Initiates Brake Channel A pin  
+}
+
+void loop() {
+  //Motor A forward @ full speed
+  digitalWrite(12, LOW); //Back = Forward pois o carro anda de ré...
+  digitalWrite(9, LOW);   //Disengage the Brake for Channel A
+  analogWrite(3, 255);   //Spins the motor on Channel A at full speed
+
+  //Motor B forward @ half speed
+  digitalWrite(13, LOW);  //Establishes backward direction of Channel B
+  digitalWrite(8, LOW);   //Disengage the Brake for Channel B
+  analogWrite(11, 123);    //Spins the motor on Channel B at half speed
+
+  
+  delay(3000);
+
+  
+  digitalWrite(9, HIGH);  //Engage the Brake for Channel A
+  digitalWrite(9, HIGH);  //Engage the Brake for Channel B
+
+
+  delay(1000);
+  
+  
+  //Motor A backward @ full speed
+  digitalWrite(12, HIGH);  //Establishes backward direction of Channel A
+  digitalWrite(9, LOW);   //Disengage the Brake for Channel A
+  analogWrite(3, 123);    //Spins the motor on Channel A at half speed
+  
+  //Motor B backward @ full speed
+  digitalWrite(13, HIGH); //Establishes forward direction of Channel B
+  digitalWrite(8, LOW);   //Disengage the Brake for Channel B
+  analogWrite(11, 255);   //Spins the motor on Channel B at full speed
+  
+  
+  delay(3000);
+  
+  
+  digitalWrite(9, HIGH);  //Engage the Brake for Channel A
+  digitalWrite(9, HIGH);  //Engage the Brake for Channel B
+  
+  
+  delay(1000);  
+}
