d5fbed1a933eeb63747d090d4be79c84ab561a3c
cleuton.sampaio@gmail.com <cleuton@MBP-de-CLEUTON.home>
2018-02-28 18:32:35 -0300

    Correção de rumo

--- robot_motor_sensors_test1/robot_motor_sensors_test1.ino
+++ robot_motor_sensors_test1/robot_motor_sensors_test1.ino
@@ -24,12 +24,13 @@ LSM303 compass;
 
 #define SONAR_NUM 4      // Number of sensors.
 #define MAX_DISTANCE 400 // Maximum distance (in cm) to ping.
+#define contaLoop 10
 
 NewPing sonar[SONAR_NUM] = {   // Sensor object array.
-  NewPing(front_trig, front_echo, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping. 
-  NewPing(rear_trig, rear_echo, MAX_DISTANCE), 
+  NewPing(front_trig, front_echo, MAX_DISTANCE), // Each sensor"s trigger pin, echo pin, and max distance to ping.
+  NewPing(rear_trig, rear_echo, MAX_DISTANCE),
   NewPing(left_trig, left_echo, MAX_DISTANCE),
-  NewPing(right_trig, right_echo, MAX_DISTANCE)  
+  NewPing(right_trig, right_echo, MAX_DISTANCE)
 };
 
 #define sonar_frontal 0
@@ -44,8 +45,9 @@ NewPing sonar[SONAR_NUM] = {   // Sensor object array.
 
 // Velocidades atuais e heading fixo:
 float heading = 0;
-int velocidadeA = velInicial;
+int velocidadeA = velInicial + 8;
 int velocidadeB = velInicial;
+int contaVezes = 0;
 
 // Positivo: Ele está virando para a direita, corrigir o esquerdo
 // Negativo: Ele está virando para a esquerda, corrigir o direito
@@ -56,27 +58,40 @@ float verificaHeading(float lastHeading) {
 
 // Corrige a trajetória de acordo com a diferença
 void corrige(float diferenca) {
-  if(diferenca > 0) {
+  if (contaVezes > 0) {
+    return;
+  }
+  if (diferenca > 0) {
     // Acelerar esquerdo
     velocidadeB += incVel;
+    velocidadeA -= incVel;
+    Serial.print("velB: ");
+    Serial.print(velocidadeB);
+    Serial.print(" ");
+    contaVezes = 0;
   }
-  else if(diferenca < 0) {
+  else if (diferenca < 0) {
     // Acelerar direito
-    velocidadeA += incVel;  
+    velocidadeA += incVel;
+    velocidadeB -= incVel;
+    Serial.print("velA: ");
+    Serial.print(velocidadeA);
+    Serial.print(" ");
+    contaVezes = 0;
   }
-  Serial.print("Corrigindo dif: "); 
-  Serial.println(diferenca);  
-  
-  andar(velocidadeA,velocidadeB);
+  Serial.print("Corrigindo dif: ");
+  Serial.println(diferenca);
+
+  andar(velocidadeA, velocidadeB);
 
 }
 
 // Estabelece direção: Por enquanto, só frente
 void direcao() {
-    // Canal A
-    digitalWrite(12, LOW); //Back = Forward o motor está ao contrário
-    // Canal B
-    digitalWrite(13, LOW);  //Establishes backward direction of Channel B
+  // Canal A
+  digitalWrite(12, LOW); //Back = Forward o motor está ao contrário
+  // Canal B
+  digitalWrite(13, LOW);  //Establishes backward direction of Channel B
 }
 
 // acao = 1: freiar (HIGH), acao = 0: soltar (LOW)
@@ -97,40 +112,53 @@ void setup() {
   Wire.begin();
   compass.init();
   compass.enableDefault();
-  compass.m_min = (LSM303::vector<int16_t>){-32767, -32767, -32767};
-  compass.m_max = (LSM303::vector<int16_t>){+32767, +32767, +32767};  
+  compass.m_min = (LSM303::vector<int16_t>) {
+    -32767, -32767, -32767
+  };
+  compass.m_max = (LSM303::vector<int16_t>) {
+    +32767, +32767, +32767
+  };
 
-  // Motores: 
+  // Motores:
   // Canal A: Motor direito (da frente do carro, que é onde está o Raspberry)
   pinMode(12, OUTPUT); //Initiates Motor Channel A pin
   pinMode(9, OUTPUT); //Initiates Brake Channel A pin
   // Canal B: Motor esquerdo
   pinMode(13, OUTPUT); //Initiates Motor Channel A pin
-  pinMode(8, OUTPUT);  //Initiates Brake Channel A pin  
+  pinMode(8, OUTPUT);  //Initiates Brake Channel A pin
 
   // Pega o Heding inicial:
   compass.read();
   heading = compass.heading();
-  Serial.print("direcao inicial: "); 
-  Serial.println(heading);  
+  Serial.print("direcao inicial: ");
+  Serial.println(heading);
 
   // Inicia um movimento reto baseado no heading atual:
   direcao();
   freios(LOW);
-  andar(velocidadeA,velocidadeB);
+  andar(velocidadeA, velocidadeB);
 }
 
 void loop() {
-  
+
   compass.read();
   float lastHeading = compass.heading();
-  Serial.print("direcao Atual: "); 
-  Serial.println(lastHeading);  
+  Serial.print("direcao Atual: ");
+  Serial.println(lastHeading);
+  Serial.print("Velocidades atuais(A B):");
+  Serial.print(velocidadeA);
+  Serial.print(", ");
+  Serial.println(velocidadeB);
 
   float diferencaAtual = verificaHeading(lastHeading);
-  
-  Serial.print("dif atual: "); 
-  Serial.println(diferencaAtual);  
+
+  Serial.print("dif atual: ");
+  Serial.println(diferencaAtual);
 
   corrige(diferencaAtual);
+  contaVezes = contaVezes + 1;
+  if (contaVezes > contaLoop) {
+    contaVezes = 0;
+  }
+  
 }
